ea1073616cfd0d63844f65a20113e2e4d6448802
Andrew Ponomarev <andrew@genx.global>
2020-10-09 15:39:56 +0000

    simulation.sh edited online with Bitbucket

--- simulation.sh
+++ simulation.sh
@@ -7,5 +7,3 @@ cd /media/ci/genx-relatives-snakemake-${BITBUCKET_COMMIT}/
 docker build -t genx-relatives-snakemake-${BITBUCKET_COMMIT}:latest -f containers/snakemake/Dockerfile .
 envsubst < /media/ag3r/funnel/bin/simulation-dry-run-template.json > /media/ag3r/funnel/bin/simulation-${BITBUCKET_COMMIT}.json
 /media/ag3r/funnel/bin/funnel task create /media/ag3r/funnel/bin/simulation-${BITBUCKET_COMMIT}.json
-
-#docker run --rm --privileged -it -v /media:/media -v /etc/localtime:/etc/localtime:ro genx-relatives-snakemake-${HASH}:latest launcher.py simulate --directory /media/ci/sim-${HASH} --real-run
