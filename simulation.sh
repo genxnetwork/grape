0ab16cef8a25569c7a6664578a5eb6cc04990536
Ubuntu <genx@genx.pvn3qw10gwqulf4q5gfincu2nh.fx.internal.cloudapp.net>
2020-10-19 13:50:14 +0000

    fix CI to get develop branch and switch to ssd

--- simulation.sh
+++ simulation.sh
@@ -2,7 +2,7 @@
 
 export BITBUCKET=$BITBUCKET_COMMIT
 
-git clone git@bitbucket.org:genxglobal/genx-relatives-snakemake.git /media/ci/genx-relatives-snakemake-${BITBUCKET_COMMIT}
+git clone --single-branch --branch develop git@bitbucket.org:genxglobal/genx-relatives-snakemake.git /media/ci/genx-relatives-snakemake-${BITBUCKET_COMMIT}
 cd /media/ci/genx-relatives-snakemake-${BITBUCKET_COMMIT}/
 docker build -t genx-relatives-snakemake-${BITBUCKET_COMMIT}:latest -f containers/snakemake/Dockerfile .
 envsubst < /media/ag3r/funnel/bin/simulation-template.json > /media/ag3r/funnel/bin/simulation-${BITBUCKET_COMMIT}.json
