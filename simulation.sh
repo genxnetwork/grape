518892359d529db8fb94ea2ed8e87ab0dcc7de98
Alexander Tischenko <sashaaero@yandex.ru>
2021-07-09 08:45:11 +0300

    Update simulation.sh

--- simulation.sh
+++ simulation.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-export BITBUCKET=$BITBUCKET_COMMIT
+GITHUB_REPO_URL=$1
+BRANCH_NAME=$2
+COMMIT_HASH=$3
 
-git clone --single-branch --branch develop git@bitbucket.org:genxglobal/genx-relatives-snakemake.git /media/ci/genx-relatives-snakemake-${BITBUCKET_COMMIT}
-cd /media/ci/genx-relatives-snakemake-${BITBUCKET_COMMIT}/
-docker build -t genx-relatives-snakemake-${BITBUCKET_COMMIT}:latest -f containers/snakemake/Dockerfile .
-envsubst < /media/ag3r/funnel/bin/simulation-template.json > /media/ag3r/funnel/bin/simulation-${BITBUCKET_COMMIT}.json
-/media/ag3r/funnel/bin/funnel task create /media/ag3r/funnel/bin/simulation-${BITBUCKET_COMMIT}.json
+export REF=$3
+
+git clone --single-branch --branch $BRANCH_NAME $GITHUB_REPO_URL /media/ci/genx-relatives-snakemake-$COMMIT_HASH > log_$COMMIT_HASH.txt
+cd /media/ci/genx-relatives-snakemake-$COMMIT_HASH/
+docker build -t genx-relatives-snakemake-$COMMIT_HASH:latest -f containers/snakemake/Dockerfile . >> log_$COMMIT_HASH.txt
+envsubst < /media/ag3r/funnel/bin/simulation-template.json > /media/ag3r/funnel/bin/simulation-$COMMIT_HASH.json >> log_$COMMIT_HASH.txt
+/media/ag3r/funnel/bin/funnel task create /media/ag3r/funnel/bin/simulation-$COMMIT_HASH.json
