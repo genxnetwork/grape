c1a44d528a1042145639c749db0b2d0fe8677f1a
Alexander Tischenko <a.tischenko@genomex.io>
2021-07-12 14:15:53 +0300

    fix simulation

--- simulation.sh
+++ simulation.sh
@@ -9,5 +9,5 @@ export REF=$3
 git clone --single-branch --branch $BRANCH_NAME $GITHUB_REPO_URL /media/ci/genx-relatives-snakemake-$COMMIT_HASH > log_$COMMIT_HASH.txt
 cd /media/ci/genx-relatives-snakemake-$COMMIT_HASH/
 docker build -t genx-relatives-snakemake-$COMMIT_HASH:latest -f containers/snakemake/Dockerfile . >> log_$COMMIT_HASH.txt
-envsubst < /media/ag3r/funnel/bin/simulation-template.json > /media/ag3r/funnel/bin/simulation-$COMMIT_HASH.json >> log_$COMMIT_HASH.txt
+envsubst < /media/ag3r/funnel/bin/simulation-template.json > /media/ag3r/funnel/bin/simulation-$COMMIT_HASH.json
 /media/ag3r/funnel/bin/funnel task create /media/ag3r/funnel/bin/simulation-$COMMIT_HASH.json
