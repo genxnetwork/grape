f9ee34faf0b285f93026cdb2ee510a1d3aefb9ab
kosar <52824960+Jahysama@users.noreply.github.com>
2022-05-12 21:08:37 +0300

    made script look better, changed Popen to run

--- scripts/plink_filter.py
+++ scripts/plink_filter.py
@@ -7,25 +7,34 @@ if __name__ == '__main__':
     bad_samples = snakemake.input['bad_samples']
     out = snakemake.params['out']
     batch = snakemake.params['batch']
-    logging.basicConfig(filename=snakemake.log[0], level=logging.DEBUG, format='%(levelname)s:%(asctime)s %(message)s')
 
-    p_freqx = subprocess.Popen(f"plink --vcf {vcf} --freqx --out plink/{out}", shell=True,
-                              stderr=subprocess.PIPE, stdout=subprocess.PIPE)
-    p_remove = subprocess.Popen(
-        f"plink --vcf {vcf} --remove {bad_samples} --make-bed --keep-allele-order --out plink/{out}",
-        shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+    logging.basicConfig(filename=snakemake.log[0],
+                        level=logging.DEBUG,
+                        format='%(levelname)s:%(asctime)s %(message)s')
 
-    stdout_freqx, stderr_freqx = p_freqx.communicate()
-    stdout_remove, stderr_remove = p_remove.communicate()
+    p_freqx = subprocess.run(f'plink --vcf {vcf} --freqx --out plink/{out}',
+                             shell=True,
+                             capture_output=True)
 
-    logging.info(f'{stdout_remove}\n{stdout_freqx}\n{stderr_remove}\n{stderr_freqx}'.decode('utf-8'))
+    p_remove = subprocess.run(f'plink --vcf {vcf} --remove {bad_samples} '
+                              f'--make-bed --keep-allele-order --out plink/{out}',
+                              shell=True,
+                              capture_output=True)
+
+    stdout_freqx = p_freqx.stdout.decode()
+    stderr_freqx = p_freqx.stderr.decode()
+    stdout_remove = p_remove.stderr.decode()
+    stderr_remove = p_remove.stdout.decode()
+
+    logging.info(f'{stdout_remove}\n{stdout_freqx}'
+                 f'\n{stderr_remove}\n{stderr_freqx}'.decode('utf-8'))
 
     if p_remove.returncode != 0 or p_freqx.returncode != 0:
         with open('pass_batches.list', 'r') as list:
             lines = list.readlines()
         with open('pass_batches.list', 'w') as list:
             for line in lines:
-                if line.strip("\n") != f"{batch}":
+                if line.strip('\n') != f'{batch}':
                     list.write(line)
                 else:
-                    print(f"removed {line}!")
+                    print(f'removed {line}!')
