628d709a6a6576fcf8928e1827519f13db56ed1f
kosar <52824960+Jahysama@users.noreply.github.com>
2022-05-12 15:27:36 +0300

    added keepgoing flag to snakemake

--- launcher.py
+++ launcher.py
@@ -411,7 +411,8 @@ if __name__ == '__main__':
             until=[args.until] if args.until is not None else [],
             use_conda=True,
             conda_prefix=args.conda_prefix,
-            envvars=['CONDA_ENVS_PATH', 'CONDA_PKGS_DIRS']
+            envvars=['CONDA_ENVS_PATH', 'CONDA_PKGS_DIRS'],
+            keepgoing=True
     ):
         raise ValueError("Pipeline failed see Snakemake error message for details")
 
