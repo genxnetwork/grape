3c5bf4d04d9a69585c1e0dd6c689b673886bcda9
kosar <52824960+Jahysama@users.noreply.github.com>
2022-05-13 10:37:00 +0300

    fixed else if logic

--- scripts/plink_filter.py
+++ scripts/plink_filter.py
@@ -31,14 +31,15 @@ if __name__ == '__main__':
     logging.info(f'{stdout_remove}\n{stdout_freqx}'
                  f'\n{stderr_remove}\n{stderr_freqx}'.decode('utf-8'))
 
-    if empty_batch_err in stderr_remove:
-        with open('pass_batches.list', 'r') as list:
-            lines = list.readlines()
-        with open('pass_batches.list', 'w') as list:
-            for line in lines:
-                if line.strip('\n') != f'{batch}':
-                    list.write(line)
-                else:
-                    print(f'Removed {line}!')
-    else:
-        raise Error(f"Rule plink_filter for batch{batch} failed with error! See {log} for details.")
+    if p_remove.returncode != 0 or p_freqx.returncode != 0:
+        if empty_batch_err in stderr_remove:
+            with open('pass_batches.list', 'r') as list:
+                lines = list.readlines()
+            with open('pass_batches.list', 'w') as list:
+                for line in lines:
+                    if line.strip('\n') != f'{batch}':
+                        list.write(line)
+                    else:
+                        print(f'removed {line}!')
+        else:
+            raise Error(f"Rule plink_filter for batch{batch} failed with error! See {log} for details.")
